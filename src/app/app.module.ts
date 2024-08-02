646481ffb1c98c295b56769ca669b134fe0258b1
ahmed fathy <af2822107@gmail.com>
2024-08-03 01:40:46 +0300

    "To Do List App"

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TodoWrapperComponent } from '../todo-wrapper/todo-wrapper.component';
+import { TodoFormComponent } from '../todo-form/todo-form.component';
+import { TodoListComponent } from '../todo-list/todo-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoWrapperComponent,
+    TodoFormComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
